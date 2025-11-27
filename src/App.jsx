7fc49c7c36305fc37f8325212f0aef09eb70393c
Alpha Kamara <bedohh@gmail.com>
2025-11-27 06:34:13 -0500

    Fix routing and remove invalid Leaderboard import

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+
 import WorldMap from "./components/WorldMap";
 import QuizPage from "./components/QuizPage";
 import LessonPage from "./pages/LessonPage";
-import SubjectsPage from "./pages/SubjectsPage";
-import AlphabetPage from "./pages/AlphabetPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
+import HomePage from "./pages/HomePage";
+import AlphabetPage from "./pages/AlphabetPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* NEW LANDING PAGE */}
         <Route path="/" element={<HomePage />} />
-
-        {/* CONTINENT EXPLORATION */}
         <Route path="/map" element={<WorldMap />} />
         <Route path="/quiz/:continent" element={<QuizPage />} />
-
-        {/* SUBJECT LEARNING */}
-        <Route path="/subjects" element={<SubjectsPage />} />
         <Route path="/lesson/:subject" element={<LessonPage />} />
-
-        {/* SPECIAL MODES */}
         <Route path="/alphabet" element={<AlphabetPage />} />
         <Route path="/leaderboard" element={<LeaderboardPage />} />
 
-        {/* 404 HANDLER */}
+        {/* 404 fallback */}
         <Route
           path="*"
           element={
             <div style={{ textAlign: "center", padding: "2rem" }}>
               <h2>404 - Page Not Found</h2>
-              <p>Return to the dashboard to continue learning.</p>
+              <p>Try returning to the Home Page.</p>
             </div>
           }
         />
