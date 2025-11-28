59a0a5055d3351cfc666f0649cb594607c3b908e
Alpha Kamara <bedohh@gmail.com>
2025-11-28 03:38:41 -0500

    Fix App routes for world, subjects, alphabet, and lessons

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./components/Home";
 import WorldMap from "./components/WorldMap";
 import QuizPage from "./components/QuizPage";
 import LessonPage from "./pages/LessonPage";
-import LeaderboardPage from "./pages/LeaderboardPage";
-import HomePage from "./pages/HomePage";
+import SubjectsPage from "./pages/SubjectsPage";
 import AlphabetPage from "./pages/AlphabetPage";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/map" element={<WorldMap />} />
-        <Route path="/quiz/:continent" element={<QuizPage />} />
-        <Route path="/lesson/:subject" element={<LessonPage />} />
-        <Route path="/alphabet" element={<AlphabetPage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
 
-        {/* 404 fallback */}
+        {/* HOME PAGE */}
+        <Route path="/" element={<Home />} />
+
+        {/* CONTINENT EXPLORATION */}
+        <Route path="/world" element={<WorldMap />} />
+        <Route path="/quiz/:continent" element={<QuizPage />} />
+
+        {/* SUBJECT SELECTION + AI LESSONS */}
+        <Route path="/subjects" element={<SubjectsPage />} />
+        <Route path="/lesson/:subject" element={<LessonPage />} />
+
+        {/* ALPHABET LAB */}
+        <Route path="/alphabet" element={<AlphabetPage />} />
+
+        {/* CATCH-ALL 404 */}
         <Route
           path="*"
           element={
             <div style={{ textAlign: "center", padding: "2rem" }}>
               <h2>404 - Page Not Found</h2>
-              <p>Try returning to the Home Page.</p>
+              <p>Return to the main menu</p>
             </div>
           }
         />
@@ -33,5 +41,3 @@ function App() {
     </BrowserRouter>
   );
 }
-
-export default App;
